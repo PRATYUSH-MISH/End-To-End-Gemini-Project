868c9ae2169a5644ae9498d9686b028cabfbfab6
PRATYUSH MISHRA <154694565+PRATYUSH-MISH@users.noreply.github.com>
2025-05-20 16:50:59 +0000

    final

--- qachat.py
+++ qachat.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## function to load Gemini Pro model and get repsonses
-model=genai.GenerativeModel("gemini-pro") 
+model=genai.GenerativeModel("models/gemini-2.0-flash-lite") 
 chat = model.start_chat(history=[])
 def get_gemini_response(question):
     
@@ -45,4 +45,3 @@ for role, text in st.session_state['chat_history']:
 
 
     
-
